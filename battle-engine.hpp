bce67a4341b5a88a5f7272b89c1ada71f859c26f
Christopher Rodriguez <chrissofer@Kit.example.org>
2014-05-29 10:25:34 -0400

    Added Combatant Class with display stats.

--- battle-engine.hpp
+++ battle-engine.hpp
@@ -6,6 +6,28 @@
 
 void greeting();
 
+class Combatant {
+  int strength;
+  int defense;
+public:
+  std::string name;
+  int health;
+  void display_stats();
+  Combatant(int, int, std::string, int);
+};
+
+
+Combatant::Combatant(int str, int def, std::string nam, int hea)
+{
+  strength = str;
+  defense = def;
+  name = nam;
+  health = hea;
+}
+
+void Combatant::display_stats() {
+  std::cout << "Name: " << name << ".\nHealth: " << health << ".\n";
+}
 
 void greeting()
 {
--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 
 int main (int argc, char * argv[])
 {
+  Combatant hero(5, 5, "hero", 5);
   greeting();
+
+  hero.display_stats();
   return 0;
 }
